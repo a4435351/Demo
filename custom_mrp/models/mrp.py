078e8195c770eaa6b2438471340049e787f9fc9f
Dyson <premkumar.dhandapani@pptservices.com>
2019-07-19 14:58:27 +0530

    Updated stock move name field values

--- custom_mrp/models/mrp.py
+++ custom_mrp/models/mrp.py
@@ -98,6 +98,7 @@ class StockMove(models.Model):
         product_id = self.env['product.product'].search([('id','=',vals['product_id'])])
         vals['storage_location_id'] = product_id.product_tmpl_id.storage_location_id.id
         vals['manufacturer_id'] = product_id.product_tmpl_id.manufacturer_id.id
+        vals['name'] = product_id.product_tmpl_id.x_studio_field_mHzKJ
         return super(StockMove, self).create(vals)
     
     @api.depends('product_uom_qty')
